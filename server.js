2b492bb28e0ccc44d9a841d1bbf7fb70e4fe9523
its-ankur <ankuratul2007@gmail.com>
2023-11-01 15:04:18 +0530

    some changes

--- server.js
+++ server.js
@@ -14,6 +14,37 @@ client.connect("mongodb+srv://ankur1037:Ankurqwerty2003@cluster0.apokfz1.mongodb
     console.log(err);
 });
 app.get('/', (req, res) => {
+    // let k;
+    // dbinstance.collection("Work").find({}).toArray().then((data) => {
+    //     k = data;
+    //     let b;
+    //     let c = k[0].i;
+    //     if (c <= k.length - 1) {
+    //         b = c;
+    //         c++;
+    //         res.render('index', { "work": k[b] });
+    //         //console.log(k[b]);
+    //         // schedule.scheduleJob('Work-a', '*/2 * * * * *', () => {
+    //         //     b = c;
+    //         //     c++;
+    //         //     res.render('index', { "work": k[b] });
+    //         //     //schedule.cancelJob('Work-a');
+    //         //     dbinstance.collection("Work").updateOne({ "i": b }, { $set: { "i": c } });
+    //         //});
+    //     }
+    //     else if (c > k.length - 1) {
+    //         b = c;
+    //         c = 1;
+    //         res.render('index', { "work": k[c] });
+    //         //console.log(k[c]);
+    //     }
+    //     dbinstance.collection("Work").updateOne({ "i": b }, { $set: { "i": c } });
+    // }).catch((err) => {
+    //     console.log(err);
+    // });
+    res.render('index');
+});
+app.get('/todo',(req,res)=>{
     let k;
     dbinstance.collection("Work").find({}).toArray().then((data) => {
         k = data;
@@ -22,7 +53,7 @@ app.get('/', (req, res) => {
         if (c <= k.length - 1) {
             b = c;
             c++;
-            res.render('index', { "work": k[b] });
+            res.render('todo', { "work": k[b] });
             //console.log(k[b]);
             // schedule.scheduleJob('Work-a', '*/2 * * * * *', () => {
             //     b = c;
@@ -35,7 +66,7 @@ app.get('/', (req, res) => {
         else if (c > k.length - 1) {
             b = c;
             c = 1;
-            res.render('index', { "work": k[c] });
+            res.render('todo', { "work": k[c] });
             //console.log(k[c]);
         }
         dbinstance.collection("Work").updateOne({ "i": b }, { $set: { "i": c } });
